993cf619bff03752879df63ee5db049fd495e44e
bcvance <81263222+bcvance@users.noreply.github.com>
2022-08-11 16:59:53 -0500

    can send twilio messages from extension!
    WOOOO

--- background.js
+++ background.js
@@ -4,6 +4,29 @@ let activated_default = false;
 
 console.log('test1');
 chrome.runtime.onInstalled.addListener(() => {
+    let url = 'https://api.twilio.com/2010-04-01/Accounts/AC91d35f2c7e8830c6f30c8dcc79b59382/Messages.json';
+    let headers = new Headers();
+    headers.set('Authorization', 'Basic ' + btoa('AC91d35f2c7e8830c6f30c8dcc79b59382' + ':' + '8aab0161f52574807fb4dd796f7d5759'));
+    headers.set('Content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    let details = {
+        'Body': 'test',
+        'From': '+18106311913',
+        'To': '+18157939677'
+    }
+    let formBody = []
+    for (let property in details) {
+        let encodedKey = encodeURIComponent(property);
+        let encodedValue = encodeURIComponent(details[property]);
+        formBody.push(encodedKey + '=' + encodedValue);
+    }
+    formBody = formBody.join('&');
+    fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: formBody
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));
     chrome.storage.sync.get({'words': words_default}, function(result) {
         console.log(result.words);
         chrome.storage.sync.set({'words': result.words}, () => {
